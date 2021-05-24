86d665787f04cc1b6c355893649f78a4d3c8486b
PhooMagoo <PhooMagoo@gmail.com>
2021-05-23 20:30:43 -0500

    Update game_releases.py

--- game_releases.py
+++ game_releases.py
@@ -1,1 +1,16 @@
+# Collect information about upcoming game releases.
+
+import bs4
+import os
+import requests
+import webbrowser
+from selenium import webdriver
+from pprint import pprint
+
+# Get the site we're gonna take info from.
+site_url = "https://gamefaqs.gamespot.com/new"
+
+# Let's open the bad boy.
+browser = webdriver.Chrome()
+browser.get(start_url)
 
